17dfce939cdb70e161f71d85096edb74b47511d8
jimmybutton <samu.liedtke@gmail.com>
2020-08-23 16:57:27 +0100

    change projects image field

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Comment
+from .models import Post, Category
 
 class PostAdmin(admin.ModelAdmin):
     pass
--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,8 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    # image = models.FilePathField(path="/img")
+    image = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
